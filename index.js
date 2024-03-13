27197445b35ff088cee5a21ad8063dfd449ff61a
mamsul <imamsulthoni.amd@gmail.com>
2024-03-13 22:21:28 +0700

    feat: remove body parser

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const path = require("path");
 
 const app = express();
 const PORT = 3000;
-
-app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
@@ -14,7 +11,7 @@ app.get("/", (req, res) => {
 app.get("/api/callback", (req, res) => {
   const data = req.query;
   res.status(200).json({ status: "success", data });
-})
+});
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
