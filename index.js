7ffa4b37f58831a71a7f4bb1f468e7071cb32814
mamsul <imamsulthoni.amd@gmail.com>
2024-03-13 14:18:20 +0700

    feat: api server and setup running script

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('test');
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/api", (_, res) => {
+  res.status(200).json({ status: "success", data: {} });
+});
+
+app.post("/callback", (req, res) => {
+  const data = req.body;
+  res.status(200).json({ status: "success", data });
+});
+
+app.listen(PORT, () => {
+  console.log(`server running on PORT ${PORT}`);
+});
