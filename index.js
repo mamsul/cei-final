aeea44bfd46822a5cf87fd5e6eecf7d127746658
mamsul <imamsulthoni.amd@gmail.com>
2024-03-13 22:08:30 +0700

    feat: modify callback route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 const PORT = 3000;
@@ -11,14 +11,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/api", (_, res) => {
-  res.status(200).json({ status: "success", data: {} });
-});
-
-app.post("/callback", (req, res) => {
-  const data = req.body;
+app.get("/api/callback", (req, res) => {
+  const data = req.query;
   res.status(200).json({ status: "success", data });
-});
+})
 
 app.listen(PORT, () => {
   console.log(`server running on PORT ${PORT}`);
